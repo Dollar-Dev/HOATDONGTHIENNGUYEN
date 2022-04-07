480564289cd656661e68036d4037a6b3cc7c8abd
Dollar <dollar.nef@gmail.com>
2022-04-07 15:14:50 +0700

    dola

--- config.php
+++ config.php
@@ -1,4 +1,4 @@
 <?php 
 define('content_path','content/');
-
+define('vendor_path','vendor/');
 ?>
--- index.php
+++ index.php
@@ -6,16 +6,16 @@ include 'config.php';
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<title>Hoạt động thiện nguyện sinh viên</title>
-	<link rel="stylesheet" href="vendor/bootstrap-4.1.1-dist/css/bootstrap.min.css" >
+	<?php echo '<link rel="stylesheet" href="'.vendor_path.'bootstrap-4.1.1-dist/css/bootstrap.min.css" >'; ?>
 	<link rel="stylesheet" href="vendor\fontawesome-free-5\css\all.min.css">
 	<link rel="stylesheet" href="css/style.css">
 </head>
-<body>	
+<body>
     <!-- Top header -->
 	<?php include content_path.'TopHeader.php'; ?>
 	<!-- Menu -->
 	<?php include content_path.'Menu.php'; ?>
-	<?php include content_path.'BodyIndex.php'; ?>s
+	<?php include content_path.'BodyIndex.php'; ?>
 	<!-- Footer -->
 	<?php include content_path.'Footer.php'; ?>
 	<!-- Nút quay về đầu trang -->
