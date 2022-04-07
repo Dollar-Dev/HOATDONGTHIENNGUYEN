c0b3391e40114e1b731bba7c404d8cee3d597175
Dollar <dollar.nef@gmail.com>
2022-04-07 15:23:06 +0700

    ddd

--- index.php
+++ index.php
@@ -5,22 +5,17 @@ include 'config.php';
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-<<<<<<< HEAD
 	<title>Hoạt động thiện nguyện sinh viên</title>lozzinh
 	<?php echo '<link rel="stylesheet" href="'.vendor_path.'bootstrap-4.1.1-dist/css/bootstrap.min.css" >'; ?>
-=======
-	<title>Hoạt động thiện nguyện sinh viên</title>
-	<link rel="stylesheet" href="vendor/bootstrap-4.1.1-dist/css/bootstrap.min.css" >
->>>>>>> 500308876d8b24c7755ab19de5a33f29f8d80f6b
 	<link rel="stylesheet" href="vendor\fontawesome-free-5\css\all.min.css">
 	<link rel="stylesheet" href="css/style.css">
 </head>
-<body>	
+<body>
     <!-- Top header -->
 	<?php include content_path.'TopHeader.php'; ?>
 	<!-- Menu -->
 	<?php include content_path.'Menu.php'; ?>
-	<?php include content_path.'BodyIndex.php'; ?>s
+	<?php include content_path.'BodyIndex.php'; ?>
 	<!-- Footer -->
 	<?php include content_path.'Footer.php'; ?>
 	<!-- Nút quay về đầu trang -->
