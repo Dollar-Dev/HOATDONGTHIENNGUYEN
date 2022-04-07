b796316f33a20b5cbd3895599235aeb00a9067ca
Luu Hang Nga <0306201354@caothang.edu.vn>
2022-04-07 13:27:18 +0700

    nga

--- catagories-post.php
+++ catagories-post.php
@@ -9,39 +9,9 @@
 </head>
 <body>
 	<!-- Top header -->
-	<?php include 'contentHeader.php'; ?>
+	<?php include "connten/TopHeader.php"; ?>
 	<!-- Menu -->
-	<div class="menu bg-light">
-		<div class="container">
-			<nav class="navbar navbar-expand-md navbar-light">			  
-				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-					<span class="navbar-toggler-icon"></span>
-				</button>
-				<div class="collapse navbar-collapse" id="navbarNavDropdown">
-					<ul class="navbar-nav w-100 justify-content-around">
-						<li class="nav-item active">
-							<a class="nav-link" href="#">Trang chủ <span class="sr-only">(current)</span></a>
-						</li>
-						<li class="nav-item">
-							<a class="nav-link" href="#">Giới thiệu</a>
-						</li>			     
-						<li class="nav-item dropdown">
-							<a class="nav-link dropdown-toggle" href="#" id="navbarDropdownHoatDong" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-								Hoạt động
-							</a>
-							<div class="dropdown-menu" aria-labelledby="navbarDropdownHoatDong">
-								<a class="dropdown-item" href="#">Chiến dịch thiện nguyện hè</a>
-								<a class="dropdown-item" href="#">Công tác giáo dục</a>			          
-							</div>
-						</li>
-						<li class="nav-item">
-							<a class="nav-link" href="#">Liên hệ</a>
-						</li>
-					</ul>
-				</div>
-			</nav>
-		</div>
-	</div>
+	<?php include "content/Menu.php"; ?>
 	<div class="container mt-3">
         <!-- Điều hướng -->
 		<div class="row">
@@ -156,53 +126,13 @@
 		<div class="container">
 			<div class="row pt-3 pb-3">
 				<!-- Logo + liên hệ -->
-				<div class="col-lg-3">
-					<div class="p-2 mr-auto">
-						<img class="img-fluid" src="img/logo.png" alt="logo website">
-						<div class="mt-3">
-							<strong>Liên hệ:</strong>
-							<br>
-							<i class="fas fa-map-marker-alt text-danger"></i>65 Huỳnh Thúc Kháng, P.Bến Nghé,
-							Q.1, TP.HCM
-						</div>
-					</div>
-				</div>
+				<?php include 'content/Logo-Lienhe.php'; ?>
 				<!-- Danh mục 1 -->
-				<div class="col-lg col-4">
-					<h5>
-					Danh mục 1</h5>
-					<ul>
-						<li>Danh mục 1.1</li>
-						<li>Danh mục 1.2</li>
-						<li>Danh mục 1.3</li>
-						<li>Danh mục 1.4</li>
-						<li>Danh mục 1.5</li>
-					</ul>
-				</div>
+				<?php include "content/Danhmuc1.php"; ?>
 				<!-- Danh mục 2 -->
-				<div class="col-lg col-4">
-					<h5>
-					Danh mục 2</h5>
-					<ul>
-						<li>Danh mục 2.1</li>
-						<li>Danh mục 2.2</li>
-						<li>Danh mục 2.3</li>
-						<li>Danh mục 2.4</li>
-						<li>Danh mục 2.5</li>
-					</ul>
-				</div>
+				<?php include "content/Danhmuc2.php"; ?>
 				<!-- Danh mục 3 -->
-				<div class="col-lg col-4">
-					<h5>
-					Danh mục 3</h5>
-					<ul>
-						<li>Danh mục 3.1</li>
-						<li>Danh mục 3.2</li>
-						<li>Danh mục 3.3</li>
-						<li>Danh mục 3.4</li>
-						<li>Danh mục 3.5</li>
-					</ul>
-				</div>
+				<?php include "content/Danhmuc3.php"; ?>
 				<!-- Đóng góp -->
 				<div class="col-lg-3 text-right">
 					<textarea class="form-control" rows="4" id="comment" placeholder="Mời bạn đóng góp ý kiến"></textarea>
---
+++ content/Danhmuc1.php
@@ -0,0 +1,14 @@
+<?php 
+echo
+"<div class="col-lg col-4">
+	<h5>
+		Danh mục 1</h5>
+			<ul>
+			<li>Danh mục 1.1</li>
+			<li>Danh mục 1.2</li>
+			<li>Danh mục 1.3</li>
+			<li>Danh mục 1.4</li>
+			<li>Danh mục 1.5</li>
+			</ul>
+</div>";
+?>
