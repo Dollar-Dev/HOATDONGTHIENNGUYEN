de1ce0fe66f71a53b1dda5c9fc1bdc80bc903932
Dollar <dollar.nef@gmail.com>
2022-04-07 14:22:37 +0700

    doo

---
+++ content/BodyDetailPost.php
@@ -0,0 +1,10 @@
+<?php 
+echo '<div class="container mt-3">
+<!-- Điều hướng -->';
+include 'DieuHuong.php';
+echo '<!-- Bài viết chi tiết và bài viết liên quan -->';
+include 'BaiVietCHiTietnLienQuan.php';
+echo '<!-- Bình luận -->';
+include 'BinhLuan.php';
+echo '</div>';
+?>
