a4ebecc7dd2cff9fd66aed591c911b774ff23186
Dollar <dollar.nef@gmail.com>
2022-04-07 12:48:39 +0700

    do

--- catagories-post.php
+++ catagories-post.php
@@ -9,32 +9,7 @@
 </head>
 <body>
 	<!-- Top header -->
-	<div id="top-header" class="top-header bg-danger">
-		<div class="container">
-			<div class="row">
-				<div class="col-12">
-					<div class="d-flex align-items-center">
-						<div class="p-2 mr-auto">
-							<img class="img-fluid" src="img/logo.png" alt="logo website">
-						</div>
-						<div class="d-flex p-2 align-items-center">
-							<div class="login mr-md-2 mr-lg-5">
-								<a class="mr-3" href="#">Đăng nhập</a> <a href="#">Đăng ký</a>
-							</div>
-							<div class="search-form">
-								<form action="#" method="post">
-									<input type="search" name="search" class="form-control" placeholder="Search">
-									<button type="submit">
-										<i class="fa fa-search" aria-hidden="true"></i>
-									</button>
-								</form>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
+	<?php include 'contentHeader.php'; ?>
 	<!-- Menu -->
 	<div class="menu bg-light">
 		<div class="container">
---
+++ content/TopHeader.php
@@ -0,0 +1,29 @@
+<?php
+echo
+'<div id="top-header" class="top-header bg-danger">
+		<div class="container">
+			<div class="row">
+				<div class="col-12">
+					<div class="d-flex align-items-center">
+						<div class="p-2 mr-auto">
+							<img class="img-fluid" src="img/logo.png" alt="logo website">
+						</div>
+						<div class="d-flex p-2 align-items-center">
+							<div class="login mr-md-2 mr-lg-5">
+								<a class="mr-3" href="#">Đăng nhập</a> <a href="#">Đăng ký</a>
+							</div>
+							<div class="search-form">
+								<form action="#" method="post">
+									<input type="search" name="search" class="form-control" placeholder="Search">
+									<button type="submit">
+										<i class="fa fa-search" aria-hidden="true"></i>
+									</button>
+								</form>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>';
+?>
