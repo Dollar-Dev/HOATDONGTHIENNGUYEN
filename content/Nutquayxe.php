ad7c7c6ba6073804061731a0e10a35bee27a5f1b
Dollar <dollar.nef@gmail.com>
2022-04-07 15:29:41 +0700

    do

--- content/Nutquayxe.php
+++
@@ -1,8 +0,0 @@
-<?php
-  include 'Nutquayvetrangdau.php'; 
-
-echo '<script src="vendor/js/jquery-3.3.1.min.js"></script>
-<script src="vendor/js/popper.min.js"></script>
-<script src="vendor/bootstrap-4.1.1-dist/js/bootstrap.min.js" ></script>
-<script src="js/script.js"></script>';
-?>
--- index.php
+++ index.php
@@ -5,7 +5,7 @@ include 'config.php';
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<title>Hoạt động thiện nguyện sinh viên</title>lozzinh
+	<title>Hoạt động thiện nguyện sinh viên</title>
 	<?php echo '<link rel="stylesheet" href="'.vendor_path.'bootstrap-4.1.1-dist/css/bootstrap.min.css" >'; ?>
 	<link rel="stylesheet" href="vendor\fontawesome-free-5\css\all.min.css">
 	<link rel="stylesheet" href="css/style.css">
@@ -19,6 +19,10 @@ include 'config.php';
 	<!-- Footer -->
 	<?php include content_path.'Footer.php'; ?>
 	<!-- Nút quay về đầu trang -->
-	<?php include content_path.'Nutquayxe.php';?>
+	<?php include content_path.'Nutquayvetrangdau.php';?>
+	<script src="vendor/js/jquery-3.3.1.min.js"></script>
+	<script src="vendor/js/popper.min.js"></script>
+	<script src="vendor/bootstrap-4.1.1-dist/js/bootstrap.min.js" ></script>
+	<script src="js/script.js"></script>
 </body>
 </html>
