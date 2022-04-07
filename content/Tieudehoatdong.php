e2588d0ba2297aee22a3de02ca67d28b940bcb1b
Dinh <0306201403@caothang.edu.vn>
2022-04-07 14:04:31 +0700

    Moi

---
+++ content/Tieudehoatdong.php
@@ -0,0 +1,10 @@
+<?php
+echo'
+<div class="row mt-4 mb-3">
+			<div class="col-12">
+				<div class="title-section">
+					<h4 class="bg-danger p-3 text-white">Hình ảnh hoạt động</h4>
+				</div>
+			</div>			
+		</div>';
+?>
