fa0574b92519292ecc95042a49db988e57f1a808
Luu Hang Nga <0306201354@caothang.edu.vn>
2022-04-07 14:03:03 +0700

    nga

--- content/Footer.php
+++ content/Footer.php
@@ -1,4 +1,22 @@
 <?php
 echo
-
+"<div class="footer mt-3 text-light">
+    <div class="container-fluid">
+    <div class="container">
+    <div class="row pt-3 pb-3">";
+    <!-- Logo + liên hệ -->
+		<?php include 'content/Logo-Lienhe.php'; ?>
+	<!-- Danh mục 1 -->
+		<?php include "content/Danhmuc1.php"; ?>
+	<!-- Danh mục 2 -->
+		<?php include "content/Danhmuc2.php"; ?>
+	<!-- Danh mục 3 -->
+		<?php include "content/Danhmuc3.php"; ?>
+	<!-- Đóng góp -->
+		<?php include "content/Donggop.php"; ?>
+echo
+"</div>
+</div>
+</div>
+</div>";
 ?>
