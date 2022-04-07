066ff57af7a8eb1141ffc718826608db83624587
Dinh <0306201403@caothang.edu.vn>
2022-04-07 15:24:01 +0700

    Moi

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ include 'config.php';
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<title>Hoạt động thiện nguyện sinh viên</title>lozzinh
+	<title>Hoạt động thiện nguyện sinh viên</title>
 	<?php echo '<link rel="stylesheet" href="'.vendor_path.'bootstrap-4.1.1-dist/css/bootstrap.min.css" >'; ?>
 	<link rel="stylesheet" href="vendor\fontawesome-free-5\css\all.min.css">
 	<link rel="stylesheet" href="css/style.css">
