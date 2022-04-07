05e1bddf58a8bab38321d9e12022937768cd86f8
Dollar <dollar.nef@gmail.com>
2022-04-07 13:59:58 +0700

    dollar

---
+++ content/BaiVietChiTiet.php
@@ -0,0 +1,13 @@
+<?php
+echo '<div class="col-lg-9 single-post">
+<img src="img/tin1.jpg" alt="Để trở thành sinh viên năng động cần như thế nào?" class="post-thumb w-100">
+<h1 class="title">
+Để trở thành sinh viên năng động cần như thế nào?</h1>
+<div>
+<span class="author"><i class="fas fa-user"></i>Sưu tầm</span> <span class="time"><i
+class="far fa-calendar-check"></i>27 Tháng Bảy, 2018</span>
+</div>
+    <!-- Nội dung bài viết -->';
+    include 'NoiDungBaiViet.php';
+echo '</div>';
+?>
