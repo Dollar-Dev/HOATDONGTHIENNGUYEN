a08162ba50dfe4aa7f4f80db71dc2f838d696915
Dinh <0306201403@caothang.edu.vn>
2022-04-07 13:35:19 +0700

    Moi

---
+++ content/Theslideshow.php
@@ -0,0 +1,16 @@
+<?php
+echo'<div class="carousel-inner">
+<div class="carousel-item active">
+    <img src="img/slide0.jpg" alt="Slide 0">
+</div>
+<div class="carousel-item">
+    <img src="img/slide1.jpg" alt="Slide 1">
+</div>
+<div class="carousel-item">
+    <img src="img/slide2.jpg" alt="Slide 2">
+</div>
+<div class="carousel-item">
+    <img src="img/slide3.jpg" alt="Slide 3">
+</div>
+</div>';
+?>
