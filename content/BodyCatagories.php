eb4337f5b325d3a8df9d5505966f9a113dccc3a1
Luu Hang Nga <0306201354@caothang.edu.vn>
2022-04-11 21:40:40 +0700

    nga

--- content/BodyCatagories.php
+++ content/BodyCatagories.php
@@ -2,13 +2,13 @@
 echo
 '<div class="container mt-3">
         <!-- Điều hướng -->';
-<?php include content_path.'Dieuhuong_catagories.php'; ?>
+<?php include 'Dieuhuong_catagories.php'; ?>
 echo 
 '<!-- Danh sách sản phẩm -->';
-<?php include content_path.'Danhsachsanpham.php'; ?>
+<?php include 'Danhsachsanpham.php'; ?>
 echo
 '<!-- Phân trang --> ';
-<?php include content_path.'Phantrang.php'; ?>
+<?php include 'Phantrang.php'; ?>
 echo 
 '</div>';
 ?>
