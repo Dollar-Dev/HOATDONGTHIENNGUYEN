5fa461c1d7f20cafde7eed8426587a2011b88e1c
Dollar <dollar.nef@gmail.com>
2022-04-11 22:07:39 +0700

    perfect

--- content/BodyCatagories.php
+++ content/BodyCatagories.php
@@ -2,13 +2,13 @@
 echo
 '<div class="container mt-3">
         <!-- Điều hướng -->';
-<?php include 'Dieuhuong_catagories.php'; ?>
+include 'Dieuhuong_catagories.php'; 
 echo 
 '<!-- Danh sách sản phẩm -->';
-<?php include 'Danhsachsanpham.php'; ?>
+include 'Danhsachsanpham.php'; 
 echo
 '<!-- Phân trang --> ';
-<?php include 'Phantrang.php'; ?>
+include 'Phantrang.php'; 
 echo 
 '</div>';
 ?>
